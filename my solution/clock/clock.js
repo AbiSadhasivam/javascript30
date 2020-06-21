49338de05bc57a6a5000e036a2496c93c2e5ac07
Abinaya Sadhasivam <abinaya.sadhasivam@solitontech.local>
2020-06-21 22:38:05 +0530

    Add current clock timing display
    Signed-off-by: Abinaya Sadhasivam <abinaya.sadhasivam@solitontech.local>

--- my solution/clock/clock.js
+++ my solution/clock/clock.js
@@ -1,27 +1,17 @@
-//minute
-//second
-//hour
-
-// 360 / 12;
-let getRotationDegree = (elementClassName) => {
-  let ele = document.getElementsByClassName(elementClassName)[0];
-  let rotationMatrix = window.getComputedStyle(ele).transform;
-  let values = rotationMatrix.split("(")[1].split(")")[0].split(",");
-  let angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
-  return [angle < 0 ? angle + 360 : angle, ele]; //adding 360 degrees here when angle < 0 is equivalent to adding (2 * Math.PI) radians before
-};
+let setTransform = (className, deg) => {
+  let ele = document.getElementsByClassName(className)[0];
+  ele.style.transform = "rotate(" + deg + "deg)";
+}
 
 setInterval(() => {
-  let [rotationDeg, ele] = getRotationDegree('second');
-  ele.style.transform = "rotate(" + (rotationDeg + 6) + "deg)";
-}, 1000);
-setInterval(() => {
-  let [rotationDeg, ele] = getRotationDegree('minute');
-  ele.style.transform = "rotate(" + (rotationDeg + 6) + "deg)";
-}, 1000 * 60 );
-setInterval(() => {
-  let [rotationDeg, ele] = getRotationDegree('hour');
-  ele.style.transform = "rotate(" + (rotationDeg + 30) + "deg)";
-}, 1000 * 60 * 60);
-
-
+  let currentTime = new Date();
+  let seconds = currentTime.getSeconds();
+  let minutes = currentTime.getMinutes();
+  let hour = currentTime.getHours()>12 ?currentTime.getHours() %12: currentTime.getHours();
+  let secondsDeg = seconds * 6 - 90; //360/60
+  let minutesDeg = minutes * 6 - 90; //360/60
+  let hourDeg = hour  * 30 - 90; // 360/12
+  setTransform('hour', hourDeg);
+  setTransform('minute', minutesDeg);
+  setTransform('second', secondsDeg);
+},1000)
